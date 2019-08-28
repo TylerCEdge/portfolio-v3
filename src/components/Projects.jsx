039fec6873f9a99f1ea52278df6a173f85ebcb72
TylerCEdge <tylercedge@gmail.com>
2019-08-28 14:20:15 -0400

    added github links all go github profile currently

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,27 +3,26 @@ import Test from './card'
 import cards from '../cards.json'
 
 export default class Projects extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            cards
-        };
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards,
     }
-    render() {
-        return (
-          <>
-          {this.state.cards.map(card => (
-            <Test
+  }
+  render() {
+    return (
+      <>
+        {this.state.cards.map(card => (
+          <Test
             id={card.id}
             key={card.id}
             image={card.image}
             name={card.name}
             url={card.url}
             summary={card.summary}
-            />
-          ))}
-          </>
-        )
-    }
+          />
+        ))}
+      </>
+    )
+  }
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,23 +2,38 @@ import React from 'react'
 import { Box, Card, Image, Heading, Text, Button, Link } from 'rebass'
 
 const Test = props => (
-	<Box>
+  <Box>
     <Card
       sx={{
         p: 1,
         borderRadius: 2,
         boxShadow: '0 0 16px rgba(0, 0, 0, .25)',
-      }}>
-      <Image alt={props.image.replace(".png", "")} src={require("../images/" + props.image)} />
+      }}
+    >
+      <Image
+        alt={props.image.replace('.png', '')}
+        src={require('../images/' + props.image)}
+      />
       <Box px={2}>
-        <Heading as='h3'>
-          {props.name}
-        </Heading>
-        <Text fontSize={0}>
-          {props.summary}
-        </Text>
+        <Heading as="h3">{props.name}</Heading>
+        <Text fontSize={0}>{props.summary}</Text>
         <br></br>
-        <Button variant='primary' mr={2}><Link variant='nav' target='blank' href={props.url}>Project</Link></Button>
+        <Button variant="primary" mr={2}>
+          <Link variant="nav" target="blank" href={props.url}>
+            View
+          </Link>
+        </Button>
+        <Button variant="primary" mr={2}>
+          <Link
+            variant="nav"
+            target="blank"
+            href="https://github.com/TylerCEdge"
+            className="icon fa-github"
+          >
+            {' '}
+            Github
+          </Link>
+        </Button>
       </Box>
     </Card>
     <hr></hr>
